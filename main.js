415a6c5357cd5bbafdf52ff4bccf86e2a299295d
Lukaisasa4 <isasanovic4@gmail.com>
2025-11-12 13:29:11 +0100

    NuevoMenu

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron'; // 👈 Añadido Menu
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+  });
 
-  win.loadURL('http://localhost:5173/')
-}
+  win.loadURL('http://localhost:5173/');
+
+  // 🔹 Menú personalizado
+  const template = [
+    {
+      label: 'Aplicación',
+      submenu: [
+        { role: 'reload', label: 'Recargar' },
+        { role: 'quit', label: 'Salir' }
+      ]
+    },
+    {
+      label: 'Ayuda',
+      submenu: [
+        { label: 'Sobre nosotros', click: () => console.log('Abrir info') }
+      ]
+    }
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+};
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
