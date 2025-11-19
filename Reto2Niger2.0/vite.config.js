6a0bd980e6446566588d9d199637613674bdefc6
Enriquecebanc <emoralescebanc@gmail.com>
2025-11-19 10:35:04 +0100

    para la maquina virtual

--- Reto2Niger2.0/vite.config.js
+++ Reto2Niger2.0/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",   // Permite acceder desde fuera del localhost
+    port: 5173         // O el puerto que uses (por defecto 5173)
+  }
 })
